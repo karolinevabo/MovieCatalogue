9196151c881a30fdfe07b190fe837833cfd3e9bd
Karoline <karoline.vabo@knowit.no>
2023-08-15 19:48:50 +0200

    Adding .js and index.html files

---
+++ MovieCatalogue/Pages/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+import SearchBar from './components/searchBar'
+
+function App() {
+    return (
+        <div classname='App'>
+            <SearchBar/>
+        </div>
+    )}export default App
---
+++ MovieCatalogue/Pages/searchBar.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react'
+const searchBar = () => {
+
+    const [searchInput, setSearchInput] = useState("");
+    
+    
+
+}
